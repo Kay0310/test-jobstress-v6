75269bc6c9a752a213fd75061c7319ccb72fce55
Kay0310 <kangyoon.kim@ihealse.com>
2025-04-20 22:28:26 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+from pathlib import Path
+
+st.set_page_config(page_title="직무스트레스 설문 시스템", layout="centered")
+
+if Path("직무스트레스.png").exists():
+    st.image("직무스트레스.png", use_container_width=True)
+else:
+    st.warning("로고 이미지가 없습니다.")
+
+st.title("직무스트레스 설문 시스템")
+st.write("안녕하세요. 본 설문은 직무스트레스 수준을 확인하기 위한 목적이며, 응답 내용은 익명으로 처리됩니다.")
